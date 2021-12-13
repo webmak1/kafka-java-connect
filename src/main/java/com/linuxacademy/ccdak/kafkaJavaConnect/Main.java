5acb5143f4ac6294db3236a4fea3a1dfdad49af2
WebMakaka <marley.nodejs@gmail.com>
2021-12-13 19:00:38 +0300

    Update for JDK 17

--- src/main/java/com/linuxacademy/ccdak/kafkaJavaConnect/Main.java
+++ src/main/java/com/linuxacademy/ccdak/kafkaJavaConnect/Main.java
@@ -1,8 +1,22 @@
 package com.linuxacademy.ccdak.kafkaJavaConnect;
 
+import java.util.Properties;
+import org.apache.kafka.clients.producer.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+       Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        for (int i = 0; i < 100; i++) {
+            producer.send(new ProducerRecord<String, String>("count-topic", "count", Integer.toString(i)));
+        }
+
+        producer.close();
     }
 }
